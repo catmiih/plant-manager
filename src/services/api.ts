d3aa75297f30f5572edac268c979cb5f747f649c
Emily Leme <emilyleme.dev@gmail.com>
2021-04-23 14:50:30 -0300

    Final

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '[https://my-json-server.typicode.com/**birobirobiro/nlw-05-plantmanager**](https://my-json-server.typicode.com/catmiih/plant-manager',
+    baseURL: 'https://my-json-server.typicode.com/catmiih/plant-manager',
 
 });
 
